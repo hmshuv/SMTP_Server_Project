1804069025c9f977713febf731ec8ca18e519577
Himanshu Gupta <himanshugupta543210@gmail.com>
2024-02-28 17:50:44 +0530

    commit

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
+const { error } = require("console");
+
 const SMTPServer  = require("smtp-server").SMTPServer;
 
 const server = new SMTPServer({
-
+   onConnect(session, cb){
+      // cb(new error('Cannot accept connection'))
+      console.log(`onConnect`, session.id);
+      cb()//accept the connection
+   },
+   onMailFrom(address, session, cb){
+      console.log(`onMailFrom`, address.address, session.id);
+      cb();
+   },
+   onRcptTo(address, session, cb){
+      console.log(`OnRcptTo`, address.address, session.id);
+      cb();
+   },
+   
 });
 
-server.listen(25, ()=> console.log("server running on port 25"));
+server.listen(25, ()=> console.log("Server running on port 25"));
